98a11d3e38d8634a7e1138260f2fb399b9846dc6
213405067 <rumeysafeyzaugrasskan@gmail.com>
2023-03-26 20:26:16 +0300

    hesap makinesi odevim

---
+++ app/src/main/java/com/lesson/hesapmakinesiodevson/Hesapla.java
@@ -0,0 +1,25 @@
+package com.lesson.hesapmakinesiodevson;
+
+public class Hesapla {
+    private int gelenSayi1, gelenSayi2;
+
+    public Hesapla(int gelenSayi1,int gelenSayi2){
+        this.gelenSayi1 = gelenSayi1;
+        this.gelenSayi2 = gelenSayi2;
+    }
+    public int toplam(){
+        return gelenSayi1 + gelenSayi2;
+    }
+    public int fark(){
+        return gelenSayi1-gelenSayi2;
+
+    }
+    public int bolum(){
+        return gelenSayi1/gelenSayi2;
+
+    }
+    public int carpim(){
+        return gelenSayi1*gelenSayi2;
+
+    }
+}
